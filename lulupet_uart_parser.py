e52f4b6d53e5e61484d9f71acf38f17adbce8217
William Hsu <williamhsu17@gmail.com>
2020-10-15 12:25:14 +0800

    add comment

--- lulupet_uart_parser.py
+++ lulupet_uart_parser.py
@@ -6,7 +6,9 @@ def lulupet_uart_string_parser(string_to_parse):
     return tuple(values.split(','))
 
 if __name__ == '__main__':
-    string_to_parse = 'lulupet1234:[1, 254.48]'
+    # string_to_parse為預計可以從termainal拿到的字串
+    # parser會找出字串lulupet1234:後面[]中的值，並用,作切分
+    string_to_parse = 'skdjhfalwkfelulupet1234:[1, 254.48]sdf,gbnalrg'
     pir, weight = lulupet_uart_string_parser(string_to_parse)
     
     print('pir: ' + pir)
